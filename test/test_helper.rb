c5d9301ed4d3bee86c19a519dcac4befae3dfb12
Frederick Cheung <frederick.cheung@gmail.com>
2009-04-11 14:05:46 +0100

    require test_process (so that tests will run against rails 2.2.2)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require 'test/unit'
 require 'active_support'
 require 'active_support/test_case'
 require 'action_controller'
+require 'action_controller/test_process'
 require 'action_controller/test_case'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + "/../lib/")
