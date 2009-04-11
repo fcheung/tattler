d8fffe00199860af25665668eea171aee53f3d32
Frederick Cheung <frederick.cheung@gmail.com>
2009-04-11 14:07:33 +0100

    don't add comments if the template isn't html or xml

--- lib/tattler.rb
+++ lib/tattler.rb
@@ -1,7 +1,12 @@
 # Tattler
 class ActionView::Template
   def render_template_with_comment(*args)
-    "<!-- TEMPLATE: #{self.relative_path} -->\n" + render_template_without_comment(*args) + "\n<!-- ENDTEMPLATE: #{self.relative_path} -->"
+    render_result = render_template_without_comment(*args)
+    if mime_type.nil? || mime_type === [Mime::HTML, Mime::XML]
+      "<!-- TEMPLATE: #{self.relative_path} -->\n" + render_result + "\n<!-- ENDTEMPLATE: #{self.relative_path} -->"
+    else
+      render_result
+    end
   end
   alias_method_chain :render_template, :comment
 end
--- test/tattler_test.rb
+++ test/tattler_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class DummyController < ActionController::Base
   def index
   end
+  
+  def json
+  end
 end
 
 class TattlerTest < ActionController::TestCase
@@ -17,5 +20,10 @@ Here is a partial
 <!-- ENDTEMPLATE: views/dummy/index.html.erb -->
     _HTML
     assert_equal expected.strip, @response.body
+  end
+  
+  test "should not affect json documents" do
+    get :json, :format => 'json'
+    assert_equal '{"foo": bar}', @response.body
   end
 end
